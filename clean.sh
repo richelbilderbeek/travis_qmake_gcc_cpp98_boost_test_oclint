d8e06685f3193daff6db6f3dd7c35744092d0e21
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-25 16:52:32 +0200

    EOD

--- clean.sh
+++ clean.sh
@@ -4,11 +4,13 @@ rm Makefile
 rm Makefile.Debug
 rm Makefile.Release
 rm *.pro.user
-rm travis_qmake_gcc_cpp98_boost_test
-rm travis_qmake_gcc_cpp98_boost_test_test
 #rm exe_debug.txt
 #rm exe_release.txt
 #rm test_debug.txt
 #rm test_release.txt
 rm -rf debug
 rm -rf release
+rm -rf oclint-0.10.3
+rm *.tar.gz
+rm travis_qmake_gcc_cpp98_boost_test_oclint
+rm travis_qmake_gcc_cpp98_boost_test_oclint_test
